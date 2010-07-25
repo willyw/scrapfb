65a3de3045af45b760b97b10a1a71ca89fb1c24d
“Willy <will@rubify.com>
2010-07-25 20:36:44 +0800

    boom boom, done

--- app/models/uploaded_item.rb
+++ app/models/uploaded_item.rb
@@ -2,6 +2,9 @@
 require 'httparty'
 require 'json'
 require 'cgi'
+require 'fileutils'
+require 'ftools'
+require 'fastercsv'
 class UploadedItem < ActiveRecord::Base
   attr_accessor :fb_link
   belongs_to :user
@@ -27,14 +30,60 @@ class UploadedItem < ActiveRecord::Base
     puts url
     while(true) do 
       response_json = self.get_response( url , hash )
-      puts response_json.inspect
+      # puts response_json.inspect
       self.print_conversation( response_json )
       unless self.any_next_page?( response_json )
         break
       end
       hash =  self.set_next_hash( response_json['paging']['next'])
     end
+    get_link
+    make_csv
   end
+  
+  
+  def get_link
+    hash = {:access_token => self.user.fb_access_token}
+    url  = URL + self.fb_node_id.to_s
+    
+    for uploaded_item_data in uploaded_item_datas.all(:limit => 3)
+     url  = URL + uploaded_item_data.data_fb_id
+     response_json = self.get_response( url, hash )
+     uploaded_item_data.data_link =  response_json["link"]
+     uploaded_item_data.save
+    end
+  end
+  
+  
+  def make_csv
+    dir_name = "#{RAILS_ROOT}/faster_csv/"
+    # file name format
+    # uploaded_item_id  + "_" + owner_id + ".csv"
+      
+   
+    dir_name = "#{RAILS_ROOT}/faster_csv/owner/#{self.user.id}/"
+    file_name  = "#{self.id}_#{self.user.id}.csv"
+    if File.exist?( dir_name + file_name )
+    else
+      FileUtils.mkdir_p( dir_name ) # create the dir and all parent directories
+      File.new(dir_name + file_name, 'w')
+    end
+    
+    FasterCSV.open(dir_name + file_name, "w") do |csv|
+      csv << ["fb_user_id", "Name", "link", "message"]
+      # csv << ["willy", "17", "M"]
+      #  csv << ["panda", "23", "F"]
+      for uploaded_item_data in uploaded_item_datas
+        array = []
+        array << uploaded_item_data.data_fb_id.to_s
+        array << uploaded_item_data.data_name
+        array << uploaded_item_data.data_link
+        array << uploaded_item_data.data_message
+        csv << array
+      end
+    end
+  end
+  
   
   def set_next_hash( next_link )
     puts "Inside set_next_hash"
